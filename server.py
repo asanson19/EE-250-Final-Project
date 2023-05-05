22594ea7a97030871003a13319580ee0441fe5a7
rcochoa <rcochoa@usc.edu>
2023-05-05 14:14:14 -0700

    fix string syntax

--- server.py
+++ server.py
@@ -77,7 +77,7 @@ def main():
         s.listen()
         conn, addr = s.accept()
         with conn:
-            print(f"Connected by {addr}")
+            print("Connected by", addr)
             while True:
                 brightness = get_brightness()
                 if brightness == "Dark":
